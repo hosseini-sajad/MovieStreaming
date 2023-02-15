d3731ce4dcc725b32f8fa56399394f671649f2d4
hosseini-sajad <hosseni_sajjad@yahoo.com>
2023-02-15 20:32:38 +0330

    Added bottom navigation with dummy data

--- app/src/main/java/com/example/moviestreaming/MainActivity.kt
+++ app/src/main/java/com/example/moviestreaming/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.moviestreaming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.moviestreaming.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        navView.setupWithNavController(navController)
     }
 }
