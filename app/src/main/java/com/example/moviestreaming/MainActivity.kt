29e02b056b2ca1474e346f7e42975bcf44266bff
hosseini-sajad <hosseni_sajjad@yahoo.com>
2023-02-18 14:07:16 -0500

    Added color and theme

--- app/src/main/java/com/example/moviestreaming/MainActivity.kt
+++ app/src/main/java/com/example/moviestreaming/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.moviestreaming
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviestreaming.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,6 +10,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
