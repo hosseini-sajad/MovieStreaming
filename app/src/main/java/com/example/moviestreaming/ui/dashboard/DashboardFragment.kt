f910a74e5f5880e226d321d1818df0ceb82f2a48
hosseini-sajad <hosseni_sajjad@yahoo.com>
2023-02-19 13:25:47 +0330

    Code cleanup

--- app/src/main/java/com/example/moviestreaming/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/moviestreaming/ui/dashboard/DashboardFragment.kt
@@ -12,9 +12,6 @@ import com.example.moviestreaming.databinding.FragmentDashboardBinding
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
