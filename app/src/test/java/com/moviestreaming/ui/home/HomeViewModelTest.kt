0edc8d0d3ba5dbd30eb5f593c83d84b8356e1fcb
hosseini-sajad <hosseni_sajjad@yahoo.com>
2023-03-10 12:21:58 -0500

    Add dummy data to home viewModel test

--- app/src/test/java/com/moviestreaming/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/com/moviestreaming/ui/home/HomeViewModelTest.kt
@@ -6,26 +6,29 @@ import org.junit.Before
 import org.junit.Test
 
 class HomeViewModelTest {
-    val trending1 = TrendingEntity(1, "x", "y", "tv")
-    val trending2 = TrendingEntity(2, "x2", "y2", "tv2")
-    val trending3 = TrendingEntity(3, "x3", "y3", "tv3")
-    val trending4 = TrendingEntity(4, "x4", "y4", "tv4")
 
-    val listOfTrending = listOf(trending1, trending2, trending3, trending4)
 
-    private lateinit var repository: FakeMovieRepository
+
+    private lateinit var movieRepository: FakeMovieRepository
     private lateinit var homeViewModel: HomeViewModel
 
     @Before
     fun createViewModel() {
-        repository = FakeMovieRepository()
-        repository.addTrending(listOfTrending)
-        homeViewModel = HomeViewModel(repository)
+        val trending1 = TrendingEntity(1, "x", "y", "tv")
+        val trending2 = TrendingEntity(2, "x2", "y2", "tv2")
+        val trending3 = TrendingEntity(3, "x3", "y3", "tv3")
+        val trending4 = TrendingEntity(4, "x4", "y4", "tv4")
+
+        val listOfTrending = listOf(trending1, trending2, trending3, trending4)
+
+        movieRepository = FakeMovieRepository()
+        movieRepository.addTrending(listOfTrending)
+        homeViewModel = HomeViewModel(movieRepository)
     }
 
     @Test
     fun get_TrendingFromRepository() {
-        
+        homeViewModel.trending
     }
 
 }
