87434502276da7c5fd19401ce3a62e44eff335ba
hosseini-sajad <hosseni_sajjad@yahoo.com>
2023-09-05 16:50:06 +0330

    Removed exoPlayer from trailer fragment

--- app/src/main/java/com/moviestreaming/ui/trailer/TrailerFragment.kt
+++ app/src/main/java/com/moviestreaming/ui/trailer/TrailerFragment.kt
@@ -1,20 +1,12 @@
 package com.moviestreaming.ui.trailer
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.upstream.DefaultDataSource
-import com.google.android.exoplayer2.util.MimeTypes
 import com.moviestreaming.databinding.FragmentTrailerBinding
-import com.moviestreaming.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -24,10 +16,6 @@ class TrailerFragment : Fragment() {
     private var _binding: FragmentTrailerBinding? = null
     private val binding get() = _binding!!
     private val args: TrailerFragmentArgs by navArgs()
-    private lateinit var simpleExoplayer: SimpleExoPlayer
-
-    private var player: ExoPlayer? = null
-    private val isPlaying get() = player?.isPlaying ?: false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,60 +27,10 @@ class TrailerFragment : Fragment() {
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-        initializePlayer()
-    }
-
-    override fun onStop() {
-        super.onStop()
-//        releasePlayer()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
-
-    private fun initializePlayer() {
-        player = ExoPlayer.Builder(requireContext()).build()
-
-        val url = Constants.YOUTUBE_URL + args.movieKey
-        Log.d("YYYYYYYY", "initializePlayer: $url")
-
-        // create a media item.
-        val mediaItem = MediaItem.Builder()
-            .setUri(url)
-            .setMimeType(MimeTypes.APPLICATION_MP4)
-            .build()
-
-        // Create a media source and pass the media item
-        val mediaSource = ProgressiveMediaSource.Factory(
-            DefaultDataSource.Factory(requireContext())
-        )
-            .createMediaSource(mediaItem)
-
-        // Finally assign this media source to the player
-        player?.apply {
-            setMediaSource(mediaSource)
-            playWhenReady = true // start playing when the exoplayer has setup
-            seekTo(0, 0L) // Start from the beginning
-            prepare() // Change the state from idle.
-        }.also {
-            binding.exoPlayer.player = it
-        }
-    }
-
-//    private fun preparePlayer(videoUrl: String, type: String) {
-//        val uri = Uri.parse(videoUrl)
-//        val mediaSource = buildMediaSource(uri, type)
-//        simpleExoplayer.prepare(mediaSource)
-//    }
-//
-//    private fun releasePlayer() {
-//        playbackPosition = simpleExoplayer.currentPosition
-//        simpleExoplayer.release()
-//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
