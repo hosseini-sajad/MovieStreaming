8b95849511facf0706ebeab2505368ad4aaba7d8
Sajad <s.hosseini107@gmail.com>
2025-05-07 06:37:01 -0400

    Added footer loading in pagination

--- app/src/main/java/com/moviestreaming/ui/category/CategoryScreen.kt
+++ app/src/main/java/com/moviestreaming/ui/category/CategoryScreen.kt
@@ -1,5 +1,6 @@
 package com.moviestreaming.ui.category
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,9 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +32,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -140,6 +145,33 @@ fun CategoryScreen(
                                         onClick = { onMovieClick(movie.id) },
                                         movie = movie
                                     )
+                                }
+                            }
+
+                            if (movies.loadState.append is LoadState.Loading) {
+                                item(span = { GridItemSpan(maxLineSpan) }) {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(16.dp),
+                                        contentAlignment = Alignment.Center
+                                    ) {
+                                        LoadingAnimation()
+                                    }
+                                }
+                            }
+
+                            movies.loadState.append.let { loadState ->
+                                if (loadState is LoadState.Error) {
+                                    item(span = { GridItemSpan(maxLineSpan) }) {
+                                        Text(
+                                            text = "Error loading more movies: ${loadState.error.localizedMessage}",
+                                            color = MaterialTheme.colorScheme.error,
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(16.dp)
+                                        )
+                                    }
                                 }
                             }
                         }
